c8bc04fd500cce56ca50a46de9f1095a0bad04d5
Fadil <rosunmungurudm@gmail.com>
2021-06-06 21:42:54 +0400

    added composer install with no system requirement check

--- www/bash/setup.sh
+++ www/bash/setup.sh
@@ -5,19 +5,25 @@ source "$(pwd)/bash/spinner.sh"
 
 # Variables
 REPO=codebase
-GIT=https://github.com/fadilxcoder/live-css-editor.git
+GIT=https://github.com/fadilxcoder/keys.git
+GIT_BRANCH=php-desktop
 
-
+# Setting up app repository
 start_spinner 'App installation...'
-
-git clone $GIT $REPO &> /dev/null # Prevent display in console
+git clone -b $GIT_BRANCH $GIT $REPO &> /dev/null # Prevent display in console
 cd $REPO
-rm -rf .git/ _config.yml chrome-capture-opt.gif README.md
+rm -rf .git/ .gitignore README.md
 cd ..
 mv index.php index.php.old
 mv $REPO/{*,.*} ./ &> /dev/null
 rm -rf $REPO
-
 stop_spinner $?
 
-sleep 5
+# PHP packages installation
+start_spinner 'Packages installation...'
+composer install --quiet --ignore-platform-reqs
+stop_spinner $?
+
+start_spinner 'System check...'
+sleep 5 # LOL
+stop_spinner $?
